fbcd26cbaf21fbe5fc2070b86c38bc2d66b5407f
Ben Turley <cameraman_ben@hotmail.com>
2014-10-31 01:44:15 +0000

    Version numbering

--- lutcalc.js
+++ lutcalc.js
@@ -13,7 +13,7 @@
 document.getElementById('javascriptwarning').style.display='none';
 var lutCalcForm = document.getElementById('lutcalcform');
 var lutInputs = new LUTInputs();
-lutInputs.addInput('version','v1');
+lutInputs.addInput('version','v1.0');
 lutInputs.addInput('date','October 2014');
 // Test for native app bridges
 if (typeof window.lutCalcApp != 'undefined') {
