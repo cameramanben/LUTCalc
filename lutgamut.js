bf89572a87143d93374d4fd7c17b94949f8c14a7
Ben Turley <cameraman_ben@hotmail.com>
2014-11-19 11:58:30 +0000

    New Canon CP Lock gamut code using Canon's matrix coefficients.

--- lutgamut.js
+++ lutgamut.js
@@ -95,7 +95,8 @@ LUTGamut.prototype.gamutList = function() {
 	this.inGamuts.push(new LUTGamutMatrix('ACES',[[1.5554591070,-0.3932807985,-0.1621783087],[0.0090216145,0.9185569566,0.0724214290],[0.0442640666,0.0118502607,0.9438856727]]));
 	this.inGamuts.push(new LUTGamutMatrix('XYZ',[[1.8467789693,-0.5259861230,-0.2105452114],[-0.4441532629,1.2594429028,0.1493999729],[0.0408554212,0.0156408893,0.8682072487]]));
 	this.inGamuts.push(new LUTGamutMatrix('Alexa Wide Gamut',[[1.024107,-0.022704,-0.001402],[0.084744,0.932204,-0.016948],[0.033062,-0.036542,1.003480]]));
-	this.inGamuts.push(new LUTGamutLut('Canon CP Lock Gamut',this.paramsCPIn()));
+	this.inGamuts.push(new LUTGamutCanonIDT('Canon CP IDT (Daylight)',{ camera: 0, day: true }));
+	this.inGamuts.push(new LUTGamutCanonIDT('Canon CP IDT (Tungsten)',{ camera: 0, day: false }));
 	this.inGamuts.push(new LUTGamutMatrix('Canon Cinema Gamut',[[1.187002652,-0.168132631,-0.018870191],[0.009561993,1.002334649,-0.01189741],[0.024939851,-0.186414783,1.161474861]]));
 	this.inGamuts.push(new LUTGamutMatrix('Passthrough',[[1,0,0],[0,1,0],[0,0,1]]));
 	this.outGamuts.push(new LUTGamutMatrix('S-Gamut3.cine',[[1,0,0],[0,1,0],[0,0,1]]));
@@ -133,7 +134,24 @@ LUTGamut.prototype.gamutList = function() {
 	this.outGamuts.push(new LUTGamutMatrix('ACES',[[0.6387886672,0.2723514337,0.0888598992],[-0.0039159061,1.0880732308,-0.0841573249],[-0.0299072021,-0.0264325799,1.0563397820]]));
 	this.outGamuts.push(new LUTGamutMatrix('XYZ',[[0.5990839208,0.2489255161,0.1024464902],[0.2150758201,0.8850685017,-0.1001443219],[-0.0320658495,-0.0276583907,1.1487819910]]));
 	this.outGamuts.push(new LUTGamutMatrix('Alexa Wide Gamut',[[0.974435,0.023802,0.001763],[-0.089226,1.071257,0.017968],[-0.035355,0.038226,0.997128]]));
-	this.outGamuts.push(new LUTGamutLut('Canon CP Lock Gamut',this.paramsCPOut()));
+	this.outGamuts.push(new LUTGamutLUT(
+		'Canon CP Lock (Daylight)',
+		{
+			format: 'cube',
+			size: 33,
+			min: [0,0,0],
+			max: [1,1,1],
+			lut: this.paramsCPDaylightOut()
+		}));
+	this.outGamuts.push(new LUTGamutLUT(
+		'Canon CP Lock (Tungsten)',
+		{
+			format: 'cube',
+			size: 33,
+			min: [0,0,0],
+			max: [1,1,1],
+			lut: this.paramsCPTungstenOut()
+		}));
 	this.outGamuts.push(new LUTGamutMatrix('Canon Cinema Gamut',[[0.840981006,0.143882203,0.015137045],[-0.00825279,0.998163089,0.010090467],[-0.019382583,0.157113995,0.862268767]]));
 	this.LA = this.outGamuts.length;
 	this.outGamuts.push(new LUTGamutLA('LA'));
@@ -212,70 +230,130 @@ LUTGamutLA.prototype.calc = function(rgb) {
 	}
 	return output;
 }
-function LUTGamutLut(name,params) {
+function LUTGamutCanonIDT(name, params) {
 	this.name = name;
-	this.hscale = params.hscale;
-	this.Red = params.Red;
-	this.Green = params.Green;
-	this.Blue = params.Blue;
-}
-LUTGamutLut.prototype.calc = function(rgb) {
-	var Rlog = this.lin2LogB(rgb[0]);
-	var Glog = this.lin2LogB(rgb[1]);
-	var Blog = this.lin2LogB(rgb[2]);
-	var Rl = parseInt(Rlog);
-	var Gl = parseInt(Glog);
-	var Bl = parseInt(Blog);
-	var Rd = 1 + Rl - Rlog;
-	var Gd = 1 + Gl - Glog;
-	var Bd = 1 + Bl - Blog;
-	var OOO = [ this.Red[Bl][Gl][Rl],this.Green[Bl][Gl][Rl],this.Blue[Bl][Gl][Rl] ];
-	var ROO = [ this.Red[Bl][Gl][Rl+1],this.Green[Bl][Gl][Rl+1],this.Blue[Bl][Gl][Rl+1] ];
-	var OGO = [ this.Red[Bl][Gl+1][Rl],this.Green[Bl][Gl+1][Rl],this.Blue[Bl][Gl+1][Rl] ];
-	var RGO = [ this.Red[Bl][Gl+1][Rl+1],this.Green[Bl][Gl+1][Rl+1],this.Blue[Bl][Gl+1][Rl+1] ];
-	var OOB = [ this.Red[Bl+1][Gl][Rl],this.Green[Bl+1][Gl][Rl],this.Blue[Bl+1][Gl][Rl] ];
-	var ROB = [ this.Red[Bl+1][Gl][Rl+1],this.Green[Bl+1][Gl][Rl+1],this.Blue[Bl+1][Gl][Rl+1] ];
-	var OGB = [ this.Red[Bl+1][Gl+1][Rl],this.Green[Bl+1][Gl+1][Rl],this.Blue[Bl+1][Gl+1][Rl] ];
-	var RGB = [ this.Red[Bl+1][Gl+1][Rl+1],this.Green[Bl+1][Gl+1][Rl+1],this.Blue[Bl+1][Gl+1][Rl+1] ];
-	var out = [];
-	out[0] = (((((OOO[0]*Rd)+(ROO[0]*(1-Rd)))*Gd)+(((OGO[0]*Rd)+(RGO[0]*(1-Rd)))*(1-Gd)))*Bd)+(((((OOB[0]*Rd)+(ROB[0]*(1-Rd)))*Gd)+(((OGB[0]*Rd)+(RGB[0]*(1-Rd)))*(1-Gd)))*(1-Bd));
-	out[1] = (((((OOO[1]*Rd)+(ROO[1]*(1-Rd)))*Gd)+(((OGO[1]*Rd)+(RGO[1]*(1-Rd)))*(1-Gd)))*Bd)+(((((OOB[1]*Rd)+(ROB[1]*(1-Rd)))*Gd)+(((OGB[1]*Rd)+(RGB[1]*(1-Rd)))*(1-Gd)))*(1-Bd));
-	out[2] = (((((OOO[2]*Rd)+(ROO[2]*(1-Rd)))*Gd)+(((OGO[2]*Rd)+(RGO[2]*(1-Rd)))*(1-Gd)))*Bd)+(((((OOB[2]*Rd)+(ROB[2]*(1-Rd)))*Gd)+(((OGB[2]*Rd)+(RGB[2]*(1-Rd)))*(1-Gd)))*(1-Bd));		
-	out[0] = this.logB2Lin(out[0]);
-	out[1] = this.logB2Lin(out[1]);
-	out[2] = this.logB2Lin(out[2]);
-	if (this.hscale) {
-	 	if (rgb[0] >= 42.689927) {
-			out[0] = out[0] * rgb[0] / 42.689927;
-		}
-	 	if (rgb[1] >= 42.689927) {
-			out[1] = out[1] * rgb[1] / 42.689927;
-		}
-	 	if (rgb[2] >= 42.689927) {
-			out[2] = out[2] * rgb[2] / 42.689927;
-		}
+	this.day = true;
+	if (typeof params.day !== 'undefined') {
+		this.day = params.day;
 	}
-	return out;
+	this.cR = [];
+	this.cG = [];
+	this.cB = [];
+	this.aR = [];
+	this.aG = [];
+	this.aB = [];
+	this.setParams();
 }
-LUTGamutLut.prototype.lin2LogB = function(lin) {
-	if (lin<0) {
-		var out = ((lin / 0.0155) + 1);
-		if (out < 0) {
-			out = 0;
-		}
-		return out;
+LUTGamutCanonIDT.prototype.setParams = function() {
+	if (this.day) {
+		this.cR = [  1.08190037262167000,-0.1802987013687820, 0.09839832874710690,
+					 1.94585453645180000,-0.5095399369373750,-0.47489567735516000,
+					-0.77808675219706800,-0.7412266070049000, 0.55789443704270100,
+					-3.27787395719078000, 0.2548784176387170, 3.45581530576474000,
+					 0.33547171397473900,-0.4335212547847600,-1.65050137344141000, 1.46581418175682,
+					 0.94464656660567600,-0.7236530991558810,-0.37107650116785700];
+		this.cG = [	-0.00858997792576314, 1.0067374011962100, 0.00185257672955608,
+					 0.08487361382964520, 0.3476269064489020, 0.00202302744639390,
+					-0.07905084140915240,-0.1794975829587160,-0.17597512335707200,
+					 2.30205579706951000,-0.6272576133852190,-2.90795250918851000,
+					 1.37002437502321000,-0.1086681585655630,-2.21150552827555000, 1.53315057595445,
+					-0.54318870669950500, 1.6379303849037600,-0.44458861683658700];
+		this.cB = [	 0.12696639806511000,-0.0118914411278690, 0.88492504306275900,
+					 1.34780279822258000, 1.0364735225736500, 0.45911328995592200,
+					-0.87815742229526800,-1.3066278750436000,-0.65860431341328300,
+					-1.44440779967030000, 0.5566765887851730, 2.18798497054968000,
+					-1.43030768398665000,-0.0388323570817641, 2.63698573112453000,-1.66598882056039,
+					 0.33450249360103000,-1.6585693073090100, 0.52195618454768500];
+		this.aR = [ 0.823223367723, 0.260915231496,-0.084138599391];
+		this.aG = [ 0.096394889772, 0.852946788690, 0.050658321584];
+		this.aB = [ 0.106008725778, 0.034764063926, 0.859227210333];
 	} else {
-		var out = ((Math.log((640 * lin) + 1)*32/(15.21321775216870*Math.log(2))) + 1);
-		if (out >= 32) {
-			out = 31.999999;
-		}
-		return out
+		this.cR = [	 0.9638030044548990,-0.1607222025706550, 0.19691919811575600,
+					 2.0344468563981900,-0.4426769314510210,-0.40798378153750900,
+					-0.6407033231292540,-0.8602427982478480, 0.31715997796744600,
+					-4.8056708010296600, 0.2711837039756700, 5.10690050495570000,
+					 0.3408958169205850,-0.4869417385078620,-2.23737935753692000, 1.96647555251297,
+					 1.3020405176624300,-1.0650311762855400,-0.39247302266737800];
+		this.cG = [ -0.0421935892309314, 1.0484595917518300,-0.00626600252090315,
+					-0.1064388968872160, 0.3629086214707810, 0.11807070047226100,
+					 0.0193542539838734,-0.1560830295432670,-0.23781164949643300,
+					 1.6791642058219800,-0.6328353271678970,-1.95984471387461000,
+					 0.9532214645628140, 0.0599085176294623,-1.66452046236246000, 1.14041188349761,
+					-0.3875526235503080, 1.1482009968551200,-0.33615394141170900];
+		this.cB = [	 0.1702950331350280,-0.0682984448537245, 0.89800341171869700,
+					 1.2210682199239900, 1.6019486592292500, 0.37759919113712400,
+					-0.8257814284875310,-1.4459086807674900,-0.92892596103534400,
+					-0.8385489974558520, 0.7580939721711600, 1.32966795243196000,
+					-1.2002190566835500,-0.2548389958451290, 2.33232411639308000,-1.86381505762773,
+					 0.1115760389564230,-1.1259331584976600, 0.75169318615728700];
+		this.aR = [ 0.842214480521, 0.232440308247,-0.074654788940];
+		this.aG = [ 0.075529615153, 0.807108792984, 0.117361591909];
+		this.aB = [ 0.094854198428,-0.036214914664, 0.941360716273];
 	}
 }
-LUTGamutLut.prototype.logB2Lin = function(log) {
-	if (log<1) {
-		return (0.0155 * log) - 0.0155;
+LUTGamutCanonIDT.prototype.clip = function(input) {
+	if (input > 65535) {
+		return 65535;
 	} else {
-		return ((Math.pow(2,(log-1)*15.21321775216870/32)-1)/640);
+		return input;
 	}
+}
+LUTGamutCanonIDT.prototype.calc = function(rgb) {
+	// Linear to C-Log ire
+	var R, G, B;
+	var offsetR = (10.1596*rgb[0])+1;
+	if (offsetR <= 0 ) {
+		R = -64/876;
+	} else {
+		R = (0.529136*Math.log(offsetR)/Math.LN10) + 0.0730597;
+	}
+	var offsetG = (10.1596*rgb[1])+1;
+	if (offsetG <= 0 ) {
+		G = -64/876;
+	} else {
+		G = (0.529136*Math.log(offsetG)/Math.LN10) + 0.0730597;
+	}
+	var offsetB = (10.1596*rgb[2])+1;
+	if (offsetB <= 0 ) {
+		B = -64/876;
+	} else {
+		B = (0.529136*Math.log(offsetB)/Math.LN10) + 0.0730597;
+	}
+	var vec = [ R,		G,		B,
+				R*G,	G*B,	B*R,
+				R*R,	G*G,	B*B,
+				R*R*G,	R*R*B,	R*G*G,
+				R*G*B,	R*B*B,	G*G*B,	G*B*B,
+				R*R*R,	G*G*G,	B*B*B ];
+	// ACES conversion stage 1 (C-Log space)
+	var mid = [];
+	mid[0] =	(this.cR[0]  * vec[0] ) + (this.cR[1]  * vec[1] ) + (this.cR[2]  * vec[2] ) +
+		  		(this.cR[3]  * vec[3] ) + (this.cR[4]  * vec[4] ) + (this.cR[5]  * vec[5] ) +
+		  		(this.cR[6]  * vec[6] ) + (this.cR[7]  * vec[7] ) + (this.cR[8]  * vec[8] ) +
+		  		(this.cR[9]  * vec[9] ) + (this.cR[10] * vec[10]) + (this.cR[11] * vec[11]) +
+		  		(this.cR[12] * vec[12]) + (this.cR[13] * vec[13]) + (this.cR[14] * vec[14]) + (this.cR[15] * vec[0]) +
+		  		(this.cR[16] * vec[16]) + (this.cR[17] * vec[17]) + (this.cR[18] * vec[18]);
+	mid[1] =	(this.cG[0]  * vec[0] ) + (this.cG[1]  * vec[1] ) + (this.cG[2]  * vec[2] ) +
+		  		(this.cG[3]  * vec[3] ) + (this.cG[4]  * vec[4] ) + (this.cG[5]  * vec[5] ) +
+		  		(this.cG[6]  * vec[6] ) + (this.cG[7]  * vec[7] ) + (this.cG[8]  * vec[8] ) +
+		  		(this.cG[9]  * vec[9] ) + (this.cG[10] * vec[10]) + (this.cG[11] * vec[11]) +
+		  		(this.cG[12] * vec[12]) + (this.cG[13] * vec[13]) + (this.cG[14] * vec[14]) + (this.cG[15] * vec[0]) +
+		  		(this.cG[16] * vec[16]) + (this.cG[17] * vec[17]) + (this.cG[18] * vec[18]);
+	mid[2] =	(this.cB[0]  * vec[0] ) + (this.cB[1]  * vec[1] ) + (this.cB[2]  * vec[2] ) +
+		  		(this.cB[3]  * vec[3] ) + (this.cB[4]  * vec[4] ) + (this.cB[5]  * vec[5] ) +
+		  		(this.cB[6]  * vec[6] ) + (this.cB[7]  * vec[7] ) + (this.cB[8]  * vec[8] ) +
+		  		(this.cB[9]  * vec[9] ) + (this.cB[10] * vec[10]) + (this.cB[11] * vec[11]) +
+		  		(this.cB[12] * vec[12]) + (this.cB[13] * vec[13]) + (this.cB[14] * vec[14]) + (this.cB[15] * vec[0]) +
+		  		(this.cB[16] * vec[16]) + (this.cB[17] * vec[17]) + (this.cB[18] * vec[18]);
+	// C-Log back to linear
+	var lin = [];
+	lin[0] = this.clip((Math.pow(10,(mid[0]-0.0730597)/0.529136)-1)/10.1596);
+	lin[1] = this.clip((Math.pow(10,(mid[1]-0.0730597)/0.529136)-1)/10.1596);
+	lin[2] = this.clip((Math.pow(10,(mid[2]-0.0730597)/0.529136)-1)/10.1596);
+	// CP -> ACES stage 2 -> S-Gamut3.cine
+	var output = [];
+	output[0] = this.clip((this.aR[0] * lin[0]) + (this.aR[1] * lin[1]) + (this.aR[2] * lin[2]));
+	output[1] = this.clip((this.aG[0] * lin[0]) + (this.aG[1] * lin[1]) + (this.aG[2] * lin[2]));
+	output[2] = this.clip((this.aB[0] * lin[0]) + (this.aB[1] * lin[1]) + (this.aB[2] * lin[2]));
+	return output;
 }
