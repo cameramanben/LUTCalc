0b457f921596a9c00bd5f91dcb03ddd2a396e9fa
Ben Turley <cameraman_ben@hotmail.com>
2014-12-28 19:22:32 +0000

    Initial code to shift gamma calculations to web workers

---
+++ lutmessage.js
@@ -0,0 +1,14 @@
+/* lutmessage.js
+* messaging object between UI and calculations / web workers for LUTCalc Web App.
+* 28th December 2014
+*
+* LUTCalc generates 1D and 3D Lookup Tables (LUTs) for video cameras that shoot log gammas, 
+* principally the Sony CineAlta line.
+*
+* By Ben Turley, http://turley.tv
+* First License: GPLv2
+* Github: https://github.com/cameramanben/LUTCalc
+*/
+function LUTmessage(inputs) {
+	this.inputs = intputs;
+}
