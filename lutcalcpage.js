e22a1c928593ce39f57d462d34accbc9b09d300d
Ben Turley <cameraman_ben@hotmail.com>
2015-02-26 10:42:19 +0000

    console response.

--- lutcalcpage.js
+++ lutcalcpage.js
@@ -1,5 +1,6 @@
 var firefoxApp = document.getElementById('firefox-app');
 firefoxApp.onclick = function() {
+console.log('clicked');
 	var manifestUrl = 'https://cameramanben.github.io/LUTCalc/manifest.webapp';
 	var req = navigator.mozApps.installPackage(manifestUrl);
 	req.onsuccess = function() {
