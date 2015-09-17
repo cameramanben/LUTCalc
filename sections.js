9003f2a3f23543a974f14d6ca2fdbbdf905d47ae
Ben Turley <cameraman_ben@hotmail.com>
2015-09-17 17:48:40 +0100

    Added embed code for Vimeo screencasts.

--- sections.js
+++ sections.js
@@ -1,18 +1,21 @@
 window.onload = function() {
 	var tocAbout = document.getElementById('toc-about');
 	var tocInstall = document.getElementById('toc-install');
+	var tocHowTo = document.getElementById('toc-howto');
 	var tocFeatures = document.getElementById('toc-features');
 	var tocFAQ = document.getElementById('toc-faq');
 	var tocTechLinks = document.getElementById('toc-tech-links');
 
 	tocAbout.removeAttribute('href');
 	tocInstall.removeAttribute('href');
+	tocHowTo.removeAttribute('href');
 	tocFeatures.removeAttribute('href');
 	tocFAQ.removeAttribute('href');
 	tocTechLinks.removeAttribute('href');
 
 	var secAbout = document.getElementById('about');
 	var secInstall = document.getElementById('install');
+	var secHowTo = document.getElementById('howto');
 	var secFeatures = document.getElementById('features');
 	var secFAQ = document.getElementById('faq');
 	var secTechLinks = document.getElementById('tech-links');
@@ -20,6 +23,7 @@ window.onload = function() {
 	function showAbout() {
 		secAbout.style.display = 'block';
 		secInstall.style.display = 'none';
+		secHowTo.style.display = 'none';
 		secFeatures.style.display = 'none';
 		secFAQ.style.display = 'none';
 		secTechLinks.style.display = 'none';
@@ -27,6 +31,15 @@ window.onload = function() {
 	function showInstall() {
 		secAbout.style.display = 'none';
 		secInstall.style.display = 'block';
+		secHowTo.style.display = 'none';
+		secFeatures.style.display = 'none';
+		secFAQ.style.display = 'none';
+		secTechLinks.style.display = 'none';
+	}
+	function showHowTo() {
+		secAbout.style.display = 'none';
+		secInstall.style.display = 'none';
+		secHowTo.style.display = 'block';
 		secFeatures.style.display = 'none';
 		secFAQ.style.display = 'none';
 		secTechLinks.style.display = 'none';
@@ -34,6 +47,7 @@ window.onload = function() {
 	function showFeatures() {
 		secAbout.style.display = 'none';
 		secInstall.style.display = 'none';
+		secHowTo.style.display = 'none';
 		secFeatures.style.display = 'block';
 		secFAQ.style.display = 'none';
 		secTechLinks.style.display = 'none';
@@ -41,6 +55,7 @@ window.onload = function() {
 	function showFAQ() {
 		secAbout.style.display = 'none';
 		secInstall.style.display = 'none';
+		secHowTo.style.display = 'none';
 		secFeatures.style.display = 'none';
 		secFAQ.style.display = 'block';
 		secTechLinks.style.display = 'none';
@@ -48,6 +63,7 @@ window.onload = function() {
 	function showTechLinks() {
 		secAbout.style.display = 'none';
 		secInstall.style.display = 'none';
+		secHowTo.style.display = 'none';
 		secFeatures.style.display = 'none';
 		secFAQ.style.display = 'none';
 		secTechLinks.style.display = 'block';
@@ -55,6 +71,7 @@ window.onload = function() {
 
 	tocAbout.onclick = function(){ showAbout(); };
 	tocInstall.onclick = function(){ showInstall(); };
+	tocHowTo.onclick = function(){ showHowTo(); };
 	tocFeatures.onclick = function(){ showFeatures(); };
 	tocFAQ.onclick = function(){ showFAQ(); };
 	tocTechLinks.onclick = function(){ showTechLinks(); };
