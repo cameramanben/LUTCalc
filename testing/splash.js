12aa6d29596466f15dbf9e2cdfb0cda4bd40d8bb
Ben Turley <ben@Bens-MacBook-Pro.local>
2017-03-06 09:28:24 +0000

    Version number.

--- testing/splash.js
+++ testing/splash.js
@@ -9,7 +9,7 @@
 * First License: GPLv2
 * Github: https://github.com/cameramanben/LUTCalc
 */
-lutInputs.addInput('version','v3Beta6');
+lutInputs.addInput('version','v3.0');
 lutInputs.addInput('date','March 2017');
 var splash = document.getElementById('javascriptwarning');
 splash.removeChild(splash.firstChild);
